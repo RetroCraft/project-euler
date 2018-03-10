7862600b3d6ad4b57d2e41ffdcb0cc3ce516d580
RetroCraft <retrocraft314@gmail.com>
2018-03-10 17:20:53 -0500

    Problem 14

---
+++ src/ca/retrocraft/projecteuler/Problem14.java
@@ -0,0 +1,29 @@
+package ca.retrocraft.projecteuler;
+
+/**
+ * Longest Collatz sequence
+ * @author James Ah Yong
+ */
+public class Problem14 {
+
+	public static void main(String[] args) {
+		long longest = 0;
+		int longestLength = 0;
+		for (long start = 1; start < 1E6; start++) {
+			long i = start;
+			int chainLength = 0;
+			while (i != 1) {
+				if (i % 2 == 0) i /= 2;
+				else i = 3 * i + 1;
+				chainLength++;
+			}
+			if (chainLength > longestLength) {
+				longest = start;
+				longestLength = chainLength;
+			}
+		}
+		
+		System.out.println(longest + ": " + longestLength);
+	}
+
+}
