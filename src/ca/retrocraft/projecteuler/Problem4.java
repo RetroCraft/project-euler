8e0b947911a5a91859577847f17cf851f83bdd38
RetroCraft <retrocraft314@gmail.com>
2018-03-09 21:41:41 -0500

    Problem 4

---
+++ src/ca/retrocraft/projecteuler/Problem4.java
@@ -0,0 +1,55 @@
+package ca.retrocraft.projecteuler;
+
+import java.util.*;
+
+/**
+ * Largest palindrome product
+ * @author James Ah Yong
+ */
+public class Problem4 {
+
+	public static void main(String[] args) {
+		int a = 999;
+		int b = 999;
+		
+		List<Integer> foundPalindromes = new ArrayList<Integer>();
+		
+		while (true) {
+			int test = a * b;
+			String total = String.valueOf(test);
+			int halfLength = Math.floorDiv(total.length(), 2);
+			String start = total.substring(0, halfLength);
+			// generate reversed end string
+			String end = "";
+			
+			for (int i = 0; i < halfLength; i++) {
+				end += total.charAt(total.length() - 1 - i);
+			}
+			
+			if (start.equals(end)) {
+				foundPalindromes.add(test);
+			}
+			
+			if (a > 100) {
+				a--;
+			} else {
+				a = 999;
+				b--;
+			}
+			
+			if (b < 100) {
+				break; 
+			}
+		}
+
+		int largest = 0;
+		for (int i = 0; i < foundPalindromes.size(); i++) {
+			if (foundPalindromes.get(i) > largest) {
+				largest = foundPalindromes.get(i);
+			}
+		}
+		
+		System.out.println(largest);
+	}
+
+}
