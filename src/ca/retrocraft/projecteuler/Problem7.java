2a598508c9fa4671f53288f7dbb12c5312c270d4
RetroCraft <retrocraft314@gmail.com>
2018-03-09 22:01:02 -0500

    Problem 7

---
+++ src/ca/retrocraft/projecteuler/Problem7.java
@@ -0,0 +1,24 @@
+package ca.retrocraft.projecteuler;
+
+/**
+ * 10 001st prime
+ * @author James Ah Yong
+ */
+public class Problem7 {
+
+	public static void main(String[] args) {
+		int n = 0;
+		for (int i = 0; i <= 10001; i++) {
+			primefinder:
+			while (true) {
+				n++;
+				for (int j = 2; j <= Math.sqrt(n); j++) {
+					if (n % j == 0) continue primefinder;
+				}
+				System.out.println(i + ": " + n);
+				break;
+			}
+		}
+	}
+
+}
