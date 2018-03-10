9e247a06d082738307564864a6fa9f58daaead91
RetroCraft <retrocraft314@gmail.com>
2018-03-10 16:16:46 -0500

    Problem 10

---
+++ src/ca/retrocraft/projecteuler/Problem10.java
@@ -0,0 +1,28 @@
+package ca.retrocraft.projecteuler;
+
+/**
+ * Summation of primes
+ * @author James Ah Yong
+ */
+public class Problem10 {
+
+	public static void main(String[] args) {
+		long total = 0;
+		long n = 1;
+		while (true) {
+			primefinder:
+			while (true) {
+				n++;
+				for (int j = 2; j <= Math.sqrt(n); j++) {
+					if (n % j == 0) continue primefinder;
+				}
+				break;
+			}
+			if (n > 2000000) break;
+			System.out.println(n);
+			total += n;
+		}
+		System.out.println(total);
+	}
+
+}
