779c3486ebf0138a1f0c845077145b000ed1ecb8
RetroCraft <retrocraft314@gmail.com>
2018-07-29 13:57:58 -0400

    Problem 15 + Binomial coefficient

--- src/ca/retrocraft/Utilities.java
+++ src/ca/retrocraft/Utilities.java
@@ -7,17 +7,17 @@ public class Utilities {
 
 	public static List<Integer> divisors(int x) {
 		List<Integer> factors = new ArrayList<Integer>();
-		
+
 		for (int j = 1; j * j <= x; j++) {
 			if (x % j == 0) {
 				factors.add(j);
 				factors.add(x / j);
 			}
 		}
-		
+
 		return factors;
 	}
-	
+
 	public static long sumInts(Collection<Integer> list) {
 		long sum = 0;
 		for (int i : list) {
@@ -25,7 +25,7 @@ public class Utilities {
 		}
 		return sum;
 	}
-	
+
 	public static BigInteger sumLongs(Collection<Long> list) {
 		BigInteger sum = new BigInteger("0");
 		for (long i : list) {
@@ -33,5 +33,17 @@ public class Utilities {
 		}
 		return sum;
 	}
-	
+
+	public static long binomial(int n, int k) {
+		if (k < 0 || k > n)
+			return 0;
+		if (k == 0 || k == n)
+			return 1;
+		k = Math.min(k, n - k);
+		long c = 1;
+		for (int i = 0; i < k; i++)
+			c = c * (n - i) / (i + 1);
+		return c;
+	}
+
 }
---
+++ src/ca/retrocraft/projecteuler/Problem015.java
@@ -0,0 +1,18 @@
+package ca.retrocraft.projecteuler;
+
+import ca.retrocraft.Utilities;
+
+/**
+ * Lattice paths
+ * 
+ * See http://mathworld.wolfram.com/LatticePath.html
+ * 
+ * @author James Ah Yong
+ */
+public class Problem015 {
+
+  public static void main(String[] args) {
+    System.out.println(Utilities.binomial(40, 20));
+  }
+
+}
