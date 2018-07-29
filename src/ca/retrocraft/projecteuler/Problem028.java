22e004b4f3f0aa537a1a7ff0e3dfe83a08968bc7
RetroCraft <retrocraft314@gmail.com>
2018-07-29 16:55:46 -0400

    Problem 28: optimize

--- src/ca/retrocraft/projecteuler/Problem028.java
+++ src/ca/retrocraft/projecteuler/Problem028.java
@@ -19,15 +19,13 @@ public class Problem028 {
     for (int i = 1; i <= Math.ceil(size / 2f); i++) {
       arm1Sum += Math.pow(2 * i - 1, 2);
     }
-    // 2n^2 - 2n - 1 from 1 to floor(size/2)
-    // 2n^2 + 1 from 1 to floor(size/2)
-    // 2n^2 + 2n + 1 from 1 to floor(size/2)
+    // 4n^2 - 2n + 1 from 1 to floor(size/2)
+    // 4n^2 + 1 from 1 to floor(size/2)
+    // 4n^2 + 2n + 1 from 1 to floor(size/2)
     long otherArmSum = 0;
-    for (int i = 1; i <= Math.floor(size / 2f); i++) {
-      int j = (2 * i) * (2 * i); // 2n^2
-      otherArmSum += j - (i * 2) + 1;
-      otherArmSum += j + 1;
-      otherArmSum += j + (i * 2) + 1;
+    for (int i = 1; i <= size / 2; i++) {
+      // sum of above 3 equations is 12x^2 + 3
+      otherArmSum += (12 * i * i) + 3;
     }
     return arm1Sum + otherArmSum;
   }
