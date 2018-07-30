42c435ff70f5721b3e39f271e975c5d6497efc15
RetroCraft <retrocraft314@gmail.com>
2018-07-29 22:46:45 -0400

    Problem 74

---
+++ src/ca/retrocraft/projecteuler/Problem074.java
@@ -0,0 +1,42 @@
+package ca.retrocraft.projecteuler;
+
+import java.util.*;
+
+/**
+ * Digit factorial chains
+ *
+ * @author James Ah Yong
+ */
+public class Problem074 extends Problem {
+
+  // precalculated factorials from 0! to 9!
+  public static int[] factorial = { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 };
+
+  public static void main() {
+    int counter = 0;
+    for (int i = 0; i < 1000000; i++) {
+      if (chainLength(i) == 60)
+        counter++;
+    }
+    System.out.println(counter);
+  }
+
+  public static int chainLength(int n) {
+    Set<Integer> chain = new HashSet<>();
+    int next = n;
+    while (!chain.contains(next)) {
+      chain.add(next);
+      next = factorialDigits(next);
+    }
+    return chain.size();
+  }
+
+  public static int factorialDigits(int n) {
+    int sum = 0;
+    String[] digits = Integer.toString(n).split("");
+    for (String digit : digits)
+      sum += factorial[Integer.parseInt(digit)];
+    return sum;
+  }
+
+}
