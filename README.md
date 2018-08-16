ffec6f669136851c40c5859c14020e157bb96c38
RetroCraft <retrocraft314@gmail.com>
2018-08-16 15:11:06 -0400

    timing documentation

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ![Badge](https://projecteuler.net/profile/RetroCraft.png)
 
+<small>Friend key: _1016354_r6MEMRkqR3xk7Ukr5onhjFWaIUwGaWxZ_</small>
+
 Solutions to the Project Euler problems, in whatever languages I feel in the mood to write in.
 
 Track how many problems have been solved on the auto-generated [markdown file](progress.md).
@@ -14,3 +16,14 @@ Mathematica (`.wls`) solutions can be run with `wolframscript` or directly from 
 These files also contain code-golfed solutions.
 
 Haskell solutions can be run with `runghc` or compiled and run for optimum speed.
+
+Build C++ solutions (and any compiled anything) to `bin/` since that gets gitignored.
+
+Assorted random esoteric languages (like APL) aren't counted towards the progress counter and are included in `esoterics/`
+
+## Timing solutions
+
+Timings are stored in comments that match `/timing: \d+ (ns|ms)/` (milliseconds because Mathematica doesn't have a nanotimer).
+One day I'll write a script that tracks these.
+
+Timing output should also be directed to stderr instead of stdout, so repeated outputs can be sent to `/dev/null`.
--- src/c++/Problem001.cpp
+++ src/c++/Problem001.cpp
@@ -8,24 +8,24 @@
   Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
-// timing: 42459 ns
+// timing: 3521 ns
 
 using namespace std;
 int main()
 {
-  // int t_repeat = 1000;
-  // auto t_start = chrono::high_resolution_clock::now();
+  int t_repeat = 1000;
+  auto t_start = chrono::high_resolution_clock::now();
 
-  // for (int _i = 0; _i < t_repeat; _i++)
-  // {
-  int sum = 0;
-  for (int i = 0; i < 1000; i++)
-    if (i % 3 == 0 || i % 5 == 0)
-      sum += i;
-  cout << sum << endl;
-  // }
+  for (int _i = 0; _i < t_repeat; _i++)
+  {
+    int sum = 0;
+    for (int i = 0; i < 1000; i++)
+      if (i % 3 == 0 || i % 5 == 0)
+        sum += i;
+    cout << sum << endl;
+  }
 
-  // auto t_end = chrono::high_resolution_clock::now();
-  // int64_t t_elapsed = chrono::duration_cast<chrono::nanoseconds>(t_end - t_start).count();
-  // cerr << "mean timing: " << t_elapsed / t_repeat << " ns \n";
+  auto t_end = chrono::high_resolution_clock::now();
+  int64_t t_elapsed = chrono::duration_cast<chrono::nanoseconds>(t_end - t_start).count();
+  cerr << "mean timing: " << t_elapsed / t_repeat << " ns \n";
 }
